3fb1250fe1b161455b1b1c57315896a35af5defb
house_mac <dilanms@outlook.com>
2024-02-05 00:42:36 +0300

    Создание сокет-сервера

---
+++ sever.py
@@ -0,0 +1,12 @@
+import socket
+
+server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST, PORT = "0.0.0.0", 8081
+server_sock.bind((HOST, PORT))
+server_sock.listen(5)
+while True:
+    sock, addr = server_sock.accept()
+    data = sock.recv(1024)  # Receive
+    data = data.upper()  # Process bytes
+    sock.sendall(data)  # Send
+
